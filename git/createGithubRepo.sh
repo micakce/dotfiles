7a5be41973364d4c9a2304febf3aeec409c67672
micakce <micakce@gmail.com>
2018-05-25 18:40:38 -0300

    Worked on aliases

--- git/createGithubRepo.sh
+++ git/createGithubRepo.sh
@@ -7,7 +7,7 @@ test -z $repo_name && echo "Repo name required." 1>&2 && exit 1
 
 curl -u 'micakce' https://api.github.com/user/repos -d "{\"name\":\"$repo_name\"}"
 
-if $readme; then
+if [ ${#readme} -gt 0 ]; then
     echo $readme > README.md
 fi
 
