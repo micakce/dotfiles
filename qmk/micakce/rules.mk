0fb12843036d8834fde06c808c319a488ad3888a
Dangelo Guanipa <micakce@gmail.com>
2024-02-21 23:11:34 -0500

    ft(qmk): add reversed layout

--- qmk/micakce/rules.mk
+++ qmk/micakce/rules.mk
@@ -1,5 +1,13 @@
-MOUSEKEY_ENABLE = yes    # Mouse keys
-RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
+# MOUSEKEY_ENABLE = yes    # Mouse keys
+# RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
+# OLED_ENABLE     = yes
+# OLED_DRIVER     = SSD1306
+# LTO_ENABLE      = yes
+
+MOUSEKEY_ENABLE = no     # Mouse keys
+RGBLIGHT_ENABLE = no
+RGB_MATRIX_ENABLE = yes    # Enable WS2812 RGB underlight.
+VIA_ENABLE      = yes    # Enable VIA
 OLED_ENABLE     = yes
-OLED_DRIVER     = SSD1306
 LTO_ENABLE      = yes
+WPM_ENABLE = yes
---
+++ qmk/micakce_rev/rules.mk
@@ -0,0 +1,5 @@
+MOUSEKEY_ENABLE = yes    # Mouse keys
+RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
+OLED_ENABLE     = yes
+LTO_ENABLE      = yes
+
