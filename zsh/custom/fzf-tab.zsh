3553c290a1ec730f086018dcdf2432a2f646f9fb
Dangelo Guanipa <dguanipa@wizards.us>
2025-05-01 20:24:27 -0300

    ft(zsh): update plugins

--- zsh/custom/fzf-tab.zsh
+++ zsh/custom/fzf-tab.zsh
@@ -1,5 +1,5 @@
 zstyle ':fzf-tab:*' popup-min-size 50 50
-zstyle ':fzf-tab:completed:*' popup-pad 30 30
 zstyle ':fzf-tab:*' fzf-bindings 'ctrl-j:accept' 'ctrl-a:toggle-all' 'tab:toggle+down' 'shift-tab:toggle+up'
+zstyle ':fzf-tab:completed:*' popup-pad 30 30
 zstyle ':fzf-tab:complete:cd:*' fzf-preview 'exa -1 --color=always $realpath'
 zstyle ':fzf-tab:complete:nvim:*' fzf-preview '[ -f $realpath ] && head $realpath || exa -1 --color=always $realpath'
