433fbd87b7d6d82e167db7e0ae48e12e00f9aa65
Dangelo Guanipa <dangelo.guanipa@emnify.com>
2025-05-24 19:07:24 -0300

    ft(LazyVim): add debugging plugins

--- LazyVim/lua/plugins/colorscheme.lua
+++ LazyVim/lua/plugins/colorscheme.lua
@@ -21,17 +21,27 @@ return {
   {
     "f-person/auto-dark-mode.nvim",
     opts = {
-      update_interval = 1000,
-      set_dark_mode = function()
-        vim.api.nvim_set_option_value("background", "dark", {})
-        vim.cmd("!sed -ibak 's/Solarized-Light.toml/Tokyonight_night.toml/g' ~/.config/alacritty/alacritty.toml")
-        vim.cmd("colorscheme tokyonight-moon")
-      end,
-      set_light_mode = function()
-        vim.api.nvim_set_option_value("background", "light", {})
-        vim.cmd("!sed -ibak 's/Tokyonight_night.toml/Solarized-Light.toml/g' ~/.config/alacritty/alacritty.toml")
-        vim.cmd("colorscheme solarized")
-      end,
+      -- update_interval = 1000,
+      -- set_dark_mode = function()
+      --   vim.api.nvim_set_option_value("background", "dark", {})
+      --   vim.cmd("!sed -ibak 's/Solarized-Light.toml/Tokyonight_night.toml/g' ~/.config/alacritty/alacritty.toml")
+      --   vim.cmd("colorscheme tokyonight-moon")
+      -- end,
+      -- set_light_mode = function()
+      --   vim.api.nvim_set_option_value("background", "light", {})
+      --   vim.cmd("!sed -ibak 's/Tokyonight_night.toml/Solarized-Light.toml/g' ~/.config/alacritty/alacritty.toml")
+      --   vim.cmd("colorscheme solarized")
+      -- end,
     },
   },
+  -- {
+  --   "projekt0n/github-nvim-theme",
+  --   name = "github-theme",
+  --   lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  --   priority = 1000, -- make sure to load this before all the other start plugins
+  --   -- config = function()
+  --   --   require("github-theme").setup({ })
+  --   --   vim.cmd("colorscheme github_dark")
+  --   -- end,
+  -- },
 }
